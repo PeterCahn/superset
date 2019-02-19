c7463ca9f736a2965b1607befe31f476fee0b64c
root <pierocann93@hotmail.it>
2019-02-19 11:47:54 +0100

    Correct path name for fixing viz.py file

--- superset/bin/entrypoint.sh
+++ superset/bin/entrypoint.sh
@@ -14,15 +14,14 @@ fi
 
 # Check URI to provide an already created set of dashboards and databases to import (the URI is the DB where the data actually resides)
 if [ -n "$SQLALCHEMY_DATA_URI" ] ; then
-        cp /etc/superset/dashboards.json /home/superset/dashboards.json
-        cp /etc/superset/datasources.yaml /home/superset/datasources.yaml
+        echo "SQLALCHEMY_DATA_URI: $SQLALCHEMY_DATA_URI"
         sed -i 's|^\([[:blank:]]*\)"sqlalchemy_uri":.*$|\1"sqlalchemy_uri": "'$SQLALCHEMY_DATA_URI'",|g' /home/superset/dashboards.json
         sed -i 's|^\([[:blank:]]*\)sqlalchemy_uri:.*$|\1sqlalchemy_uri: '$SQLALCHEMY_DATA_URI'|g' /home/superset/datasources.yaml
 fi
 
 
 # Fix "Timestamped data with PostgreSQL backend" for version 0.28.1 (issue: https://github.com/apache/incubator-superset/issues/6284)
-sed -i -e 's/utc=False/utc=True/g' /usr/local/lib/python3.5/dist-packages/superset/viz.py
+sed -i -e 's/utc=False/utc=True/g' /usr/local/lib/python3.6/site-packages/superset/viz.py
 
 superset-init &
 
