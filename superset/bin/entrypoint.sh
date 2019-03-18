17b5b9db1bac7bcac7f5c4fc713d5b2f5e93467b
Pietro Cannalire <pierocann93@hotmail.it>
2019-03-18 14:35:01 +0100

    Cleaner initialization and entrypoint scripts. Avoid errors caused by not ready backend DB.

--- superset/bin/entrypoint.sh
+++ superset/bin/entrypoint.sh
@@ -1,24 +1,8 @@
 #!/bin/bash
 
-#echo "[entrypoint.sh] Starting Superset..."
+echo "[entrypoint.sh] Starting Superset..."
 
-if [ -z "$SUPERSET_CONFIG_PATH" ] ; then
-	export SUPERSET_CONFIG_PATH=/usr/local/lib/python3.6/site-packages/superset/superset_config.py
-        cp -f /usr/local/bin/custom-config.py $SUPERSET_CONFIG_PATH
-else
-	if [ ! -f $SUPERSET_CONFIG_PATH ] ; then
-		echo "superset_config.py not found in $SUPERSET_CONFIG_PATH"
-		exit
-	fi
-fi
-
-# Fix "Timestamped data with PostgreSQL backend" for version 0.28.1 (issue: https://github.com/apache/incubator-superset/issues/6284)
-sed -i -e 's/utc=False/utc=True/g' /usr/local/lib/python3.6/site-packages/superset/viz.py
-
-# Fix error when exporting yaml from datasource
-sed -i 's/key=lambda k: sorted(k.items()))/key=lambda k: (k.items()))/' /usr/local/lib/python3.6/site-packages/superset/models/helpers.py
-
-superset-init &
+superset-init
 
 echo "[entrypoint.sh] Calling CMD: $@..."
 
